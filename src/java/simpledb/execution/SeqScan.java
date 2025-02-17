dfa84cdd8a6d4a026e04a59ebc537c0a2636f4c2
hzd <2685273579@qq.com>
2025-02-17 09:41:32 +0800

    lab1 exercise6

--- src/java/simpledb/execution/SeqScan.java
+++ src/java/simpledb/execution/SeqScan.java
@@ -20,6 +20,11 @@ public class SeqScan implements OpIterator {
 
     private static final long serialVersionUID = 1L;
 
+    private final TransactionId transactionId;
+    private int tableId;
+    private String tableAlias;
+    private DbFileIterator dbFileIterator;
+
     /**
      * Creates a sequential scan over the specified table as a part of the
      * specified transaction.
@@ -38,6 +43,9 @@ public class SeqScan implements OpIterator {
      */
     public SeqScan(TransactionId tid, int tableid, String tableAlias) {
         // some code goes here
+        this.transactionId = tid;
+        this.tableId = tableid;
+        this.tableAlias = tableAlias;
     }
 
     /**
@@ -46,7 +54,7 @@ public class SeqScan implements OpIterator {
      *       be the actual name of the table in the catalog of the database
      * */
     public String getTableName() {
-        return null;
+        return Database.getCatalog().getTableName(tableId);
     }
 
     /**
@@ -55,7 +63,7 @@ public class SeqScan implements OpIterator {
     public String getAlias()
     {
         // some code goes here
-        return null;
+        return tableAlias;
     }
 
     /**
@@ -72,6 +80,8 @@ public class SeqScan implements OpIterator {
      */
     public void reset(int tableid, String tableAlias) {
         // some code goes here
+        this.tableId = tableid;
+        this.tableAlias = tableAlias;
     }
 
     public SeqScan(TransactionId tid, int tableId) {
@@ -80,6 +90,8 @@ public class SeqScan implements OpIterator {
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        dbFileIterator = Database.getCatalog().getDatabaseFile(tableId).iterator(transactionId);
+        dbFileIterator.open();
     }
 
     /**
@@ -94,26 +106,44 @@ public class SeqScan implements OpIterator {
      */
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        // 获取原始的 TupleDesc
+        TupleDesc original = Database.getCatalog().getTupleDesc(tableId);
+
+        // 创建新的字段类型数组和字段名称数组
+        Type[] typeAr = new Type[original.numFields()];
+        String[] fieldAr = new String[original.numFields()];
+
+        // 为每个字段添加表的别名前缀
+        for (int i = 0; i < original.numFields(); i++) {
+            typeAr[i] = original.getFieldType(i);
+            // 为字段名称加上前缀
+            fieldAr[i] = tableAlias + "." + original.getFieldName(i);
+        }
+
+        // 返回新的 TupleDesc，其中字段名称包含表的别名
+        return new TupleDesc(typeAr, fieldAr);
+        // return Database.getCatalog().getTupleDesc(tableId);
     }
 
     public boolean hasNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return false;
+        return dbFileIterator.hasNext();
     }
 
     public Tuple next() throws NoSuchElementException,
             TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        return dbFileIterator.next();
     }
 
     public void close() {
         // some code goes here
+        dbFileIterator = null;
     }
 
     public void rewind() throws DbException, NoSuchElementException,
             TransactionAbortedException {
         // some code goes here
+        dbFileIterator.rewind();
     }
 }
